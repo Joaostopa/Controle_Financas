dc7d79a4d3129010411a032cd3f35a1856eabf92
JoaoStopa <joao.vitorstopa@hotmail.com>
2024-12-13 13:19:20 -0300

    comit

--- app.js
+++ app.js
@@ -5,22 +5,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./src/routes/userRoutes');
 const incomeRoutes = require('./src/routes/incomeRoutes');
-const expenseRoutes = require('./src/routes/expenseRoutes'); 
-const app = express();
+const expenseRoutes = require('./src/routes/expenseRoutes');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swager.json');
+const swaggerDocument = require('./docs/swager.json'); 
+
+const app = express();
+
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+
 app.use(bodyParser.json());
+
 
 app.use('/users', userRoutes);
 app.use('/income', incomeRoutes);
-app.use('/expenses', expenseRoutes); 
+app.use('/expenses', expenseRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('API Controle de Finanças está rodando!');
 });
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+const PORT = process.env.PORT || 3002;
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
